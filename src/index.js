ac23c7df33ec9d934b72a23db979926856f54f5c
João Correia <joaocorreiaprof@gmail.com>
2024-05-31 14:45:50 +0100

    Divided content

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import { header } from "./header";
 import { Todos } from "./todo";
 
 header();
-let todo = new Todos("Clean", "clean the fuucntion house");
-todo.sayInfo();
+// let todo = new Todos("Clean", "clean the fuucntion house");
+// todo.sayInfo();
