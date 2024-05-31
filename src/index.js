a7eda60f7e2e3e1ff511ac8ab835eea3b69bdf09
João Correia <joaocorreiaprof@gmail.com>
2024-05-31 22:19:58 +0100

    Fixed modal interaction. Working now!

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+// index.js
+
 import "./style.css";
 import { logoTextCont, buttonTodo } from "./sideBar";
-import { Todos } from "./todo";
-import { initModal, openModal, closeModal } from "./modalButton.js";
+import { openModal } from "./modalButton.js";
 
 function displaySideBar() {
   logoTextCont();
   buttonTodo();
+
+  const addBtn = document.querySelector("[data-modal-target='modal']");
+  addBtn.addEventListener("click", () => {
+    const modal = document.querySelector("#modal");
+    openModal(modal);
+  });
 
   // let todo = new Todos("Clean", "clean the fuucntion house");
   // todo.sayInfo();
