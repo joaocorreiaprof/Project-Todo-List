2f60ebd6a6e26dc03c6ed9f34dddbe25379d8810
João Correia <joaocorreiaprof@gmail.com>
2024-06-15 16:15:26 +0100

    Added option to create todos
    Note that the todos are not being save locally yet

--- src/rightContent.js
+++ src/rightContent.js
@@ -1,5 +1,3 @@
-import { Project } from "./createProject";
-
 function addProjectSelectedListeners() {
   const projectSelected = document.querySelectorAll(".defaultProject");
   const displayProject = document.querySelector(".selectedProject");
@@ -13,10 +11,11 @@ function addProjectSelectedListeners() {
     let projects = JSON.parse(localStorage.getItem("projects")) || [];
     projects.forEach((projectData) => {
       if (projectData.title == displayProject.textContent) {
-        projectData.todo.push("Hello World");
+        //projectData.todo.push("Hello World");
+        createTodo();
       }
     });
-    localStorage.setItem("projects", JSON.stringify(projects)); // Atualizar o localStorage com os dados modificados
+    localStorage.setItem("projects", JSON.stringify(projects));
   });
 
   displayTodos.appendChild(buttonTodo);
@@ -25,6 +24,48 @@ function addProjectSelectedListeners() {
     button.addEventListener("click", () => {
       displayProject.innerHTML = button.innerHTML;
     });
+  });
+}
+
+function createTodo() {
+  const displayTodos = document.querySelector(".todosProject");
+
+  // create input
+  const newTodo = document.createElement("input");
+  newTodo.placeholder = "Enter your todo name:";
+  newTodo.setAttribute("required", "");
+  newTodo.classList.add("newTodo");
+  displayTodos.appendChild(newTodo);
+
+  //create input accept button
+
+  const inputAccept = document.createElement("button");
+  inputAccept.textContent = "Check";
+  displayTodos.appendChild(inputAccept);
+
+  inputAccept.addEventListener("click", () => {
+    const todoText = newTodo.value.trim();
+    if (todoText !== "") {
+      const todoButton = document.createElement("button");
+      todoButton.textContent = todoText;
+      displayTodos.appendChild(todoButton);
+
+      displayTodos.removeChild(newTodo);
+      displayTodos.removeChild(inputAccept);
+      displayTodos.removeChild(inputCancel);
+    }
+  });
+
+  //create input cancel
+
+  const inputCancel = document.createElement("button");
+  inputCancel.textContent = "Cancel";
+  displayTodos.appendChild(inputCancel);
+
+  inputCancel.addEventListener("click", () => {
+    displayTodos.removeChild(newTodo);
+    displayTodos.removeChild(inputAccept);
+    displayTodos.removeChild(inputCancel);
   });
 }
 
